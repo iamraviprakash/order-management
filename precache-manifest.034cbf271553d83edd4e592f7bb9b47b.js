7198d85e692423e701e716b5e42798bbf16f623f
iamraviprakash <raviprakash.coder@gmail.com>
2019-12-16 09:14:14 +0530

    Updates

---
+++ precache-manifest.034cbf271553d83edd4e592f7bb9b47b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aa2ec31c03a6942f6eb6838b405a502",
+    "url": "/order-management/index.html"
+  },
+  {
+    "revision": "d741508a3b7b2bda0709",
+    "url": "/order-management/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "aa27e313ff76f3a07d75",
+    "url": "/order-management/static/css/main.0682aeed.chunk.css"
+  },
+  {
+    "revision": "d741508a3b7b2bda0709",
+    "url": "/order-management/static/js/2.fd46c701.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/order-management/static/js/2.fd46c701.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa27e313ff76f3a07d75",
+    "url": "/order-management/static/js/main.bd650f76.chunk.js"
+  },
+  {
+    "revision": "32d6591d96460c9e1a72",
+    "url": "/order-management/static/js/runtime-main.2d8a9d05.js"
+  }
+]);
